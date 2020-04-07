bbc2adf268d934e03f57136320ecfda7b5617eb2
CommonMatter <hoffmelanie@gmail.com>
2020-04-07 01:14:29 -0400

    only swap images in chrome

---
+++ image-swap-google-fiona/README.md
@@ -0,0 +1,10 @@
+image-swap
+==========
+
+A simple Chrome extension that replaces images on the web with those of your choosing
+
+To install, download directory somewhere on your computer,
+then open the Chrome Extensions settings and check "Enable
+Developer Mode", then "Load Unpacked Extension" and select the
+directory.
+# imageSwap
---
+++ image-swap-google-fiona/swap.js
@@ -0,0 +1,33 @@
+﻿var images = [
+    "https://media.giphy.com/media/l41YgOPSjVe47TTRm/giphy.gif",
+    "https://media.giphy.com/media/3o7buctjKD8g5r4544/giphy.gif",
+    "https://media.giphy.com/media/xUA7b2takylWgFwJYQ/giphy.gif",
+    "https://media.giphy.com/media/3og0IAWqxqQ1aSwrde/giphy.gif",
+    "https://media.giphy.com/media/VCma6o7KQ4H4Y/giphy.gif",
+    "https://media.giphy.com/media/xT0BKEbS571e53UIDe/giphy.gif",
+    "https://media.giphy.com/media/qV4Ioi4eUpvaw/giphy.gif",
+    "https://media.giphy.com/media/hbB8efOHAezRK/giphy.gif",
+    "https://media.giphy.com/media/26BRQjZejh2vgXPJ6/giphy.gif",
+    "https://media.giphy.com/media/3o7TKxv46QYGO9ZhQY/giphy.gif",
+    "https://media.giphy.com/media/l2YOlSrNnoKC15RmM/giphy.gif",
+    "https://media.giphy.com/media/l3978y5HqiEtqupiM/giphy.gif",
+    "https://media.giphy.com/media/5xtDarvMK3GwzmY6CVa/giphy.gif",
+    "https://media.giphy.com/media/OdScEhtNTQEko/giphy.gif",
+    "https://media.giphy.com/media/uvCS7HUFIUGVG/giphy.gif",
+    "https://media.giphy.com/media/2bBVx7AAR9drW/giphy.gif"
+];
+
+window.onload = replaceImages;
+
+function replaceImages() {
+    for (var i in document.images) {
+        var targetImage = document.images[i];
+        var randomImage = images[Math.floor(Math.random() * images.length)];
+        var height = targetImage.height;
+        var width = targetImage.width;
+
+        targetImage.src = randomImage;
+        targetImage.height = height;
+        targetImage.width = width;
+    }
+}
